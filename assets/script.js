efc566290ae89858134b1d8fbb81e38f7d357be5
Michael Souter <souterm1@gmail.com>
2021-06-27 22:57:43 -0400

    Portfolio is mobile-first, and I have built in the correct media queries to account for larger screens

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,26 @@ $("#aboutBtn").on("click", function () {
   $("#about-me").show();
   $("#projects").hide();
   $("#contact-me").hide();
+  $("#skills").hide();
+});
+
+$("#skillsBtn").on("click", function () {
+  $("#about-me").hide();
+  $("#projects").hide();
+  $("#contact-me").hide();
+  $("#skills").show();
 });
 
 $("#projectBtn").on("click", function () {
   $("#about-me").hide();
   $("#projects").show();
   $("#contact-me").hide();
+  $("#skills").hide();
 });
 
 $("#contactBtn").on("click", function () {
   $("#about-me").hide();
   $("#projects").hide();
   $("#contact-me").show();
+  $("#skills").hide();
 });
